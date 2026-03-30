3bbfbc763687d18e7fc450ed13facd5ffcc74d51
mahmoudkalimero1100-rgb <mahmoudkalimero1100@gmail.com>
2026-03-30 11:38:56 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,5 @@
+### 🐍 Utilisation avec Python
+Pour tester la base de données rapidement, vous pouvez utiliser le script fourni :
+1. Téléchargez `quran.mega.tafsir.db` (depuis les Releases).
+2. Placez `example.py` dans le même dossier.
+3. Lancez : `python example.py`
